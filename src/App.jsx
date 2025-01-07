cedd8fa3b1cc1efc3837f6ad32c2dac8bfd6a80d
Anastasiya <kavalenka.anastasiya@student.ehu.lt>
2025-01-07 22:15:58 +0400

    done 5 task

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,16 @@
 import React, { Component } from 'react';
-import Header from './components/Header';
-import MainSection from './components/MainSection';
-import Footer from './components/Footer';
+import HomePage from './components/HomePage/HomePage'; 
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      itemCount: 0,
-    };
-  }
-
-  incrementItemCount = (quantity) => {
-    this.setState((prevState) => ({
-      itemCount: prevState.itemCount + quantity,
-    }));
-  };
-
   render() {
     return (
       <div className="App">
-        <Header itemCount={this.state.itemCount} />
-        <MainSection incrementItemCount={this.incrementItemCount} />
-        <Footer />
+        <HomePage />
       </div>
     );
   }
 }
 
 export default App;
+
